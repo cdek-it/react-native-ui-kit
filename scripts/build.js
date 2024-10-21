be3d616bf6a6a4b63bcf84de93cfc2e1a6f3deaf
Соколовский Иван Павлович <ivan3177@gmail.com>
2024-10-21 16:28:24 +0300

    ci(publish): add library publishing
    Publish alpha version to npm from MR

--- scripts/build.js
+++ scripts/build.js
@@ -4,5 +4,3 @@ const { exec } = require('child_process')
 
 exec('rm -rf dist')
 exec('npx yarn tsc -p tsconfig.json')
-exec('mkdir -p dist/assets/fonts')
-exec("cp -r assets/fonts dist/assets/fonts")
