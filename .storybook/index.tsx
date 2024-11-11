234bdec6d069d8611a67fac00d73b7c5f0fa0e26
Соколовский Иван Павлович <i.sokolovskii@cdek.ru>
2024-11-11 10:10:23 +0300

    feat(storybook): change storybook theme for larger text sizes
    Add theme to getStorybookUI call, add custom styles for typography size

--- .storybook/index.tsx
+++ .storybook/index.tsx
@@ -6,6 +6,22 @@ const StorybookUIRoot = view.getStorybookUI({
     getItem: AsyncStorage.getItem,
     setItem: AsyncStorage.setItem,
   },
+  theme: {
+    typography: {
+      size: {
+        s1: 20,
+        s2: 24,
+        s3: 28,
+        m1: 20,
+        m2: 24,
+        m3: 28,
+        l1: 20,
+        l2: 24,
+        l3: 28,
+        code: 30,
+      },
+    },
+  },
 });
 
 export default StorybookUIRoot;
