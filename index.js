69dc240563161e9d4bf4dca0b79b4a1e39117785
Nikita Grevtsov <n.grevtsov@cdek.ru>
2024-11-29 23:09:43 +0500

    build: downgrade reanimated version
    Should be reverted after resolving the issue
    https://github.com/software-mansion/react-native-reanimated/issues/6645

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { registerRootComponent } from 'expo'
-import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated'
+// import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated'
 
 import App from './.storybook'
 
-configureReanimatedLogger({
-  level: ReanimatedLogLevel.warn,
-  strict: false,
-})
+// TODO uncomment this on updating to reanimated >= 3.16.*
+// configureReanimatedLogger({
+//   level: ReanimatedLogLevel,
+//   strict: false,
+// })
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
