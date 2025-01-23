6841f60f84a57f1af4ea51f9481d04eede52b8e7
Sokolovskii Ivan <i.sokolovskii@cdek.ru>
2025-01-23 16:57:36 +0300

    feat(ProgressBar): реализация компонента ProgressBar

---
+++ src/components/ProgressBar/ProgressBar.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ProgressBar } from './ProgressBar'
+
+const meta: Meta<typeof ProgressBar> = {
+  title: 'ProgressBar',
+  component: ProgressBar,
+  args: {
+    showValue: false,
+    value: 20,
+    style: {
+      height: 7,
+    },
+  },
+  parameters: {
+    notes: `
+      Компонент ProgressBar
+
+      <ProgressBar
+        value={20}
+        showValue={false}
+        style={{ height: 10, width: 200 }}
+      />
+    `,
+  },
+}
+
+export default meta
+
+const ProgressBarStory: StoryObj<typeof ProgressBar> = {}
+
+export { ProgressBarStory as ProgressBar }
