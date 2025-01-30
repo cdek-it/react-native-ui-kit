d843f6c1fc0d1e7a2bd04f2d0483bf27210eb42e
Василий Клецкин <vas@RND1-000006.express.local>
2025-01-30 18:43:31 +0300

    test(checkbox): add snapshots

---
+++ src/components/Checkbox/__tests__/Checkbox.test.tsx
@@ -0,0 +1,77 @@
+import { render } from '@testing-library/react-native'
+import React from 'react'
+
+import { Checkbox, type CheckboxProps } from '../Checkbox'
+
+describe('Checkbox', () => {
+  const defaultProps: CheckboxProps = {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onPress: () => {},
+  }
+
+  describe('snapshots', () => {
+    const snapshotCases: Array<[string, Partial<CheckboxProps>]> = [
+      ['default', {}],
+      [
+        'checked = false, indeterminate = false, state = default',
+        {
+          checked: false,
+          indeterminate: false,
+          state: 'default',
+        },
+      ],
+      [
+        'checked = true, indeterminate = false, state = default',
+        {
+          checked: true,
+          indeterminate: false,
+          state: 'default',
+        },
+      ],
+      [
+        'checked = true, indeterminate = true, state = default',
+        {
+          checked: true,
+          indeterminate: true,
+          state: 'default',
+        },
+      ],
+      [
+        'checked = false, indeterminate = false, state = disabled',
+        {
+          checked: false,
+          indeterminate: false,
+          state: 'disabled',
+        },
+      ],
+      [
+        'checked = true, indeterminate = false, state = disabled',
+        {
+          checked: true,
+          indeterminate: false,
+          state: 'disabled',
+        },
+      ],
+      [
+        'checked = false, indeterminate = false, state = danger',
+        {
+          checked: false,
+          indeterminate: false,
+          state: 'danger',
+        },
+      ],
+      [
+        'checked = true, indeterminate = false, state = danger',
+        {
+          checked: true,
+          indeterminate: false,
+          state: 'danger',
+        },
+      ],
+    ]
+    test.each(snapshotCases)('%s', (_, props) => {
+      const { toJSON } = render(<Checkbox {...defaultProps} {...props} />)
+      expect(toJSON()).toMatchSnapshot()
+    })
+  })
+})
