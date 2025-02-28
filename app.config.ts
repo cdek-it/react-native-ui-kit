c706aa779731681daf5e7f464919241d2316d4b9
Sokolovskii Ivan <i.sokolovskii@cdek.ru>
2025-02-28 10:15:39 +0300

    chore(expo): добавил динамическую конфигурацию expo
    Для динамической генерации версии

---
+++ app.config.ts
@@ -0,0 +1,32 @@
+import type { ExpoConfig } from 'expo/config'
+
+import { version } from './package.json'
+
+export default {
+  expo: {
+    name: 'CDEK UI',
+    slug: 'cdek-uikit',
+    version: version,
+    orientation: 'portrait',
+    android: {
+      package: 'ru.cdek.uikit.prime',
+    },
+    ios: {
+      bundleIdentifier: 'ru.cdek.uikit.prime',
+      config: {
+        usesNonExemptEncryption: false,
+      },
+    },
+    splash: {
+      image: './splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#FFFFFF',
+    },
+    icon: './icon.png',
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    newArchEnabled: true,
+    assetBundlePatterns: ['**/*'],
+  } satisfies ExpoConfig,
+}
