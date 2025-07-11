ba3702e6ba1652bc51e8abcc64ddc00c836b8872
Nikita Grevtsov <n.grevtsov@cdek.ru>
2025-07-11 12:54:46 +0500

    fix(Dialog): добавлен экспорт
    Задача: PLATFORM-1284

--- src/components/Dialog/index.ts
+++ src/components/Dialog/index.ts
@@ -1,2 +1,3 @@
 export { Dialog, type DialogProps } from './Dialog'
 export { DialogComponent, type DialogComponentProps } from './DialogComponent'
+export { DialogHeader, type DialogHeaderProps } from './DialogHeader'
