a73aafa8651cd36067a9cb957d625f5bf0e28387
Sokolovskii Ivan <ivan3177@gmail.com>
2025-12-17 19:45:43 +0300

    chore(eslint): поправлена основная часть замечаний линтера и подправлены правила

--- src/components/Input/InputOtp/InputOtp.tsx
+++ src/components/Input/InputOtp/InputOtp.tsx
@@ -91,7 +91,7 @@ export const InputOtp = memo<InputOtpProps>(
     )
 
     const renderArray = useMemo(
-      () => Array.from({ length }).fill(null),
+      () => Array.from({ length }, (_, i) => `Otp-Item-${i}`),
       [length]
     )
 
@@ -106,13 +106,12 @@ export const InputOtp = memo<InputOtpProps>(
         {({ pressed }) => (
           <>
             <View style={styles.content}>
-              {renderArray.map((_, index) => (
+              {renderArray.map((key, index) => (
                 <InputOtpItem
                   disabled={disabled}
                   error={error}
                   focused={isFocused ? index === activeIndex : false}
-                  // eslint-disable-next-line react/no-array-index-key
-                  key={index}
+                  key={key}
                   pressed={pressed}
                   testID={`${testID}Item`}
                   value={value[index]}
