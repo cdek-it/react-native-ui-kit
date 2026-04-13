adf7e7a20f4dcd1a6daac19d2639a2d7d164538a
Рагозин Никита Александрович <n.ragozin@cdek.ru>
2026-04-13 14:46:18 +0500

    test: обновил снапшоты, добавил новые моки

--- jest.d.ts
+++ jest.d.ts
@@ -3,3 +3,9 @@ type PropertyCombinations<T> = { [K in keyof T]: Array<T[K]> }
 declare let generatePropsCombinations: <T>(
   properties: PropertyCombinations<T>
 ) => T[]
+
+declare module '@react-native/normalize-colors' {
+  const normalizeColors: (color: string | number) => number | null
+
+  export default normalizeColors
+}
