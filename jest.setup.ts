a8b049d765f1af210eb0d917b1b1fbf1176a2c78
Рагозин Никита Александрович <n.ragozin@cdek.ru>
2026-04-13 14:46:18 +0500

    test: обновил снапшоты, добавил новые моки

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,55 @@
 import 'jest-extended'
 import 'react-native-gesture-handler/jestSetup'
 import { setUpTests } from 'react-native-reanimated'
+import 'react-native-unistyles/mocks'
 
 setUpTests()
+
+type ThemeName = 'light' | 'dark'
+
+interface MockedUnistylesModule {
+  StyleSheet: { create: (...args: unknown[]) => unknown }
+  UnistylesRuntime: {
+    miniRuntime: unknown
+    setTheme: jest.Mock<void, [ThemeName]>
+    themeName: ThemeName | undefined
+  }
+  useUnistyles: jest.Mock
+  withUnistyles: jest.Mock
+}
+
+const { darkTheme, lightTheme } = require('./src/theme')
+
+const unistyles = jest.requireMock(
+  'react-native-unistyles'
+) as MockedUnistylesModule
+
+const getTheme = (themeName: ThemeName | undefined) =>
+  themeName === 'dark' ? darkTheme : lightTheme
+
+const originalStyleSheetCreate = unistyles.StyleSheet.create
+const runtime = unistyles.UnistylesRuntime
+
+runtime.themeName = 'light'
+runtime.setTheme = jest.fn((themeName: ThemeName) => {
+  runtime.themeName = themeName
+})
+
+unistyles.useUnistyles = jest.fn(() => ({
+  theme: getTheme(runtime.themeName),
+  rt: runtime,
+}))
+
+unistyles.StyleSheet.create = jest.fn(
+  (stylesheet: Parameters<typeof originalStyleSheetCreate>[0]) =>
+    typeof stylesheet === 'function'
+      ? originalStyleSheetCreate(() =>
+          stylesheet(getTheme(runtime.themeName), runtime.miniRuntime)
+        )
+      : originalStyleSheetCreate(stylesheet)
+)
+
+unistyles.withUnistyles = jest.fn(<T>(Component: T) => Component)
 
 generatePropsCombinations = <T>(properties: PropertyCombinations<T>): T[] => {
   const keys = Object.keys(properties) as Array<keyof T>
