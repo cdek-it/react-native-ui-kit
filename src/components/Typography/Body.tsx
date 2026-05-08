1154494b700c55987d0a329d14150ccf3f7527e8
Nikita Grevtsov <n.a.grevtsov@gmail.com>
2026-05-08 07:57:34 +0500

    feat(typography): изменены размеры шрифтов, PT Sans заменен на Noto Sans

--- src/components/Typography/Body.tsx
+++ src/components/Typography/Body.tsx
@@ -38,7 +38,7 @@ export const Body = ({
 
 const styles = StyleSheet.create(({ theme, typography, fonts }) => ({
   text: {
-    fontSize: typography.Size['text-lg'],
+    fontSize: typography.Size['text-base'],
     includeFontPadding: false,
     verticalAlign: 'middle',
     fontFamily: fonts.secondary,
@@ -49,8 +49,8 @@ const styles = StyleSheet.create(({ theme, typography, fonts }) => ({
   default: { color: theme.General.textColor },
   primary: { color: theme.General.primaryColor },
   secondary: { color: theme.General.textSecondaryColor },
-  base: { fontSize: typography.Size['text-base'] },
-  paragraph: { fontSize: typography.Size['text-lg'] },
-  paragraphBase: { fontSize: typography.Size['text-base'] },
+  base: { fontSize: typography.Size['text-sm'] },
+  paragraph: { lineHeight: 24 },
+  paragraphBase: { lineHeight: 21 },
   disabled: { opacity: 0.6 },
 }))
