879480f436a55cc4b225f587e9e5ee94ad06e8c0
Nikita Grevtsov <n.a.grevtsov@gmail.com>
2026-06-09 16:20:44 +0500

    fix(InputOtp): реализована синхронизация состояния disabled с реальным инпутом

--- src/components/Input/InputOtp/__tests__/InputOtp.test.tsx
+++ src/components/Input/InputOtp/__tests__/InputOtp.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render } from '@testing-library/react-native'
+import type { TextInput } from 'react-native'
 
 import { InputOtp, type InputOtpProps } from '../InputOtp'
 
@@ -42,5 +43,106 @@ describe('InputOtp component tests', () => {
     fireEvent.changeText(hiddenInput, '55   ')
 
     expect(mockedOnChange).toHaveBeenCalledWith('55')
+  })
+
+  test('should set hidden input editable prop correctly', () => {
+    const mockedOnChange = jest.fn()
+    const { getByTestId, update } = render(
+      <InputOtp length={4} testID='InputOtp' onChange={mockedOnChange} />
+    )
+
+    expect(getByTestId('InputOtpHiddenInput')).toHaveProp('editable', true)
+
+    update(
+      <InputOtp
+        disabled
+        length={4}
+        testID='InputOtp'
+        onChange={mockedOnChange}
+      />
+    )
+
+    expect(getByTestId('InputOtpHiddenInput')).toHaveProp('editable', false)
+
+    update(
+      <InputOtp
+        editable={false}
+        length={4}
+        testID='InputOtp'
+        onChange={mockedOnChange}
+      />
+    )
+
+    expect(getByTestId('InputOtpHiddenInput')).toHaveProp('editable', false)
+  })
+
+  test('should blur and reset focus when input becomes disabled', () => {
+    const mockedOnChange = jest.fn()
+    let inputRef: TextInput | null = null
+    const handleInputRef = (ref: TextInput | null) => {
+      inputRef = ref
+    }
+    const { getByTestId, getByText, queryByText, update } = render(
+      <InputOtp
+        inputRef={handleInputRef}
+        length={4}
+        testID='InputOtp'
+        onChange={mockedOnChange}
+      />
+    )
+
+    fireEvent(getByTestId('InputOtpHiddenInput'), 'focus')
+
+    expect(getByText('|')).toBeOnTheScreen()
+
+    if (!inputRef) {
+      throw new Error('Input ref was not set')
+    }
+
+    const blur = jest.fn()
+
+    Object.assign(inputRef, { blur, isFocused: () => true })
+
+    update(
+      <InputOtp
+        disabled
+        inputRef={handleInputRef}
+        length={4}
+        testID='InputOtp'
+        onChange={mockedOnChange}
+      />
+    )
+
+    expect(blur).toHaveBeenCalledOnce()
+    expect(queryByText('|')).not.toBeOnTheScreen()
+  })
+
+  test('should not focus hidden input on press when input is not editable', () => {
+    const mockedOnChange = jest.fn()
+    let inputRef: TextInput | null = null
+    const handleInputRef = (ref: TextInput | null) => {
+      inputRef = ref
+    }
+    const { getByTestId } = render(
+      <InputOtp
+        editable={false}
+        inputRef={handleInputRef}
+        length={4}
+        testID='InputOtp'
+        onChange={mockedOnChange}
+      />
+    )
+
+    if (!inputRef) {
+      throw new Error('Input ref was not set')
+    }
+
+    const focus = jest.fn()
+
+    Object.assign(inputRef, { focus })
+
+    fireEvent.press(getByTestId('InputOtp'))
+
+    expect(focus).not.toHaveBeenCalled()
   })
 })
