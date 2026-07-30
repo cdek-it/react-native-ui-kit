f1ee7522e006d949cfe9d6e9726de838a8e525b3
Alesya Volosach <alesya.volosach@gmail.com>
2026-07-30 20:28:27 +0300

    feat: добавлены и подключены дизайн-токены

--- src/theme/lightTheme.ts
+++ src/theme/lightTheme.ts
@@ -3,9 +3,13 @@ import { ModalSize } from './assets/ModalSize'
 import { customLight } from './assets/customLight'
 import lightThemeAssets from './assets/themeLight.json'
 import { commonTheme } from './commonTheme'
+import lightComponentTokens from './tokens/components/light.json'
+import lightSemanticTokens from './tokens/semantic/light.json'
 import type { ThemeType } from './types'
 
 export const lightTheme: ThemeType = {
+  semantic: lightSemanticTokens,
+  components: lightComponentTokens,
   theme: { ...lightThemeAssets, InputSize, ModalSize, custom: customLight },
   ...commonTheme,
   fonts: { primary: 'TT Fellows', secondary: 'Noto Sans' },
