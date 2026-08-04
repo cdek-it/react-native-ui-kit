c9c79286a7fceb5e094acd5bfa81d40854bc0ed0
Alesya Volosach <alesya.volosach@gmail.com>
2026-08-04 17:00:02 +0300

    feat(Theme): подключены сгенерированные токены

--- src/theme/ThemeContext.tsx
+++ src/theme/ThemeContext.tsx
@@ -6,17 +6,54 @@ import { SkeletonContextProvider } from '../utils/SkeletonContext'
 
 import { darkTheme } from './darkTheme'
 import { lightTheme } from './lightTheme'
-import { type FontsConfig, ThemeVariant } from './types'
+import {
+  componentTokens,
+  fontTokens,
+  semanticTokens,
+  type ComponentTokens,
+  type SemanticTokens,
+} from './tokens'
+import {
+  type FontsConfig,
+  type FontTokens,
+  type ThemeType,
+  ThemeVariant,
+} from './types'
+
+type InternalTheme = Omit<ThemeType, 'fonts' | 'semantic'> & {
+  components: ComponentTokens
+  fonts: FontsConfig & FontTokens
+  semantic: ThemeType['semantic'] & SemanticTokens
+}
+
+const createInternalTheme = (
+  theme: ThemeType,
+  components: ComponentTokens
+): InternalTheme => ({
+  ...theme,
+  components,
+  fonts: {
+    ...fontTokens,
+    ...theme.fonts,
+    fontFamily: { base: theme.fonts.secondary, heading: theme.fonts.primary },
+  },
+  semantic: { ...semanticTokens, ...theme.semantic },
+})
+
+const internalThemes = {
+  light: createInternalTheme(lightTheme, componentTokens.light),
+  dark: createInternalTheme(darkTheme, componentTokens.dark),
+}
 
 StyleSheet.configure({
   settings: { initialTheme: 'light' },
-  themes: { light: lightTheme, dark: darkTheme },
+  themes: internalThemes,
 })
 
 declare module 'react-native-unistyles' {
   export interface UnistylesThemes {
-    light: typeof lightTheme
-    dark: typeof darkTheme
+    light: (typeof internalThemes)['light']
+    dark: (typeof internalThemes)['dark']
   }
 }
 
@@ -42,8 +79,17 @@ export const ThemeContextProvider = ({
     UnistylesRuntime.setTheme(THEME_NAME_MAP[initialTheme])
 
     if (fonts) {
-      UnistylesRuntime.updateTheme('light', (t) => ({ ...t, fonts }))
-      UnistylesRuntime.updateTheme('dark', (t) => ({ ...t, fonts }))
+      const updateFonts = (theme: InternalTheme): InternalTheme => ({
+        ...theme,
+        fonts: {
+          ...theme.fonts,
+          ...fonts,
+          fontFamily: { base: fonts.secondary, heading: fonts.primary },
+        },
+      })
+
+      UnistylesRuntime.updateTheme('light', updateFonts)
+      UnistylesRuntime.updateTheme('dark', updateFonts)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps -- initial* props применяем только на mount
   }, [])
