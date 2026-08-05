cc97004b56df75fb303c7bbdb5b5d56e0f82dc6e
Alesya Volosach <alesya.volosach@gmail.com>
2026-08-05 12:43:53 +0300

    build: настроены APK и TestFlight через GitLab

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,52 @@ import type { ExpoConfig } from 'expo/config'
 
 import { version } from './package.json'
 
+const internalFontsPlugin = [
+  'expo-font',
+  {
+    android: {
+      fonts: [
+        {
+          fontFamily: 'TT Fellows',
+          fontDefinitions: [
+            {
+              path: './assets/fonts/TTFellows-DemiBoldItalic.ttf',
+              weight: 600,
+              style: 'italic',
+            },
+            { path: './assets/fonts/TTFellows-DemiBold.ttf', weight: 600 },
+            { path: './assets/fonts/TTFellows-Regular.ttf', weight: 400 },
+          ],
+        },
+        {
+          fontFamily: 'Noto Sans',
+          fontDefinitions: [
+            { path: './assets/fonts/NotoSans-Bold.ttf', weight: 700 },
+            { path: './assets/fonts/NotoSans-Regular.ttf', weight: 400 },
+          ],
+        },
+      ],
+    },
+    ios: {
+      fonts: [
+        './assets/fonts/TTFellows-DemiBold.ttf',
+        './assets/fonts/TTFellows-DemiBoldItalic.ttf',
+        './assets/fonts/TTFellows-Regular.ttf',
+        './assets/fonts/NotoSans-Regular.ttf',
+        './assets/fonts/NotoSans-Bold.ttf',
+      ],
+    },
+  },
+] satisfies NonNullable<ExpoConfig['plugins']>[number]
+
+const plugins: NonNullable<ExpoConfig['plugins']> = [
+  './expo/plugins/withEnsureBundler.js',
+  '@react-native-async-storage/expo-with-async-storage',
+  ...(process.env.UI_KIT_INTERNAL_FONTS === 'true'
+    ? [internalFontsPlugin]
+    : []),
+]
+
 export default {
   expo: {
     name: 'CDEK UI',
@@ -10,6 +56,7 @@ export default {
     orientation: 'portrait',
     android: { package: 'ru.cdek.uikit.prime' },
     ios: {
+      appleTeamId: 'AY3Q95T9QG',
       bundleIdentifier: 'ru.cdek.uikit.prime',
       config: { usesNonExemptEncryption: false },
     },
@@ -20,10 +67,7 @@ export default {
     },
     icon: './icon.png',
     updates: { fallbackToCacheTimeout: 0 },
-    plugins: [
-      './expo/plugins/withEnsureBundler.js',
-      '@react-native-async-storage/expo-with-async-storage',
-    ],
+    plugins,
     assetBundlePatterns: ['**/*'],
   } satisfies ExpoConfig,
 }
